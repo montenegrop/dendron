06c795281656c56c5e7590d87f50137dee958c8d
montenegrop <pablo.rpm@gmail.com>
2022-04-29 10:29:58 -0300

    timeout tests 2

--- coderHouse/timeout.js
+++ coderHouse/timeout.js
@@ -1,7 +1,7 @@
 function delay(time) {
-    return new Promise(resolve => setTimeout(resolve, time));
+    return new Promise((resolve) => setTimeout(resolve, time))
 }
-  
+
 // async function test1() {
 //     console.log('start 4 timer');
 //     await delay(4000);
@@ -10,25 +10,25 @@ function delay(time) {
 
 async function test2() {
     setTimeout(() => {
-        console.log("8000 inicial")
-    }, 8000);
+        console.log('8000 inicial')
+    }, 8000)
     setTimeout(() => {
-        console.log("2.0 interrumpe")
-    }, 2000);
+        console.log('2.0 interrumpe')
+    }, 2000)
     setTimeout(() => {
-        console.log("5.9 interrumpe")
-    }, 5900);
-    console.log('start 3.9 timer');
-    await delay(3900);
-    console.log("after 3.9");
-    await delay(4000);
-    console.log('after 7.9 second');
+        console.log('5.9 interrumpe')
+    }, 5900)
+    console.log('start 3.9 timer')
+    await delay(3900)
+    console.log('after 3.9')
+    await delay(4000)
+    console.log('after 7.9 second')
     setTimeout(() => {
-        console.log("8000 Final")
-    }, 8000);
+        console.log('8000 Final')
+    }, 8000)
 }
 
 // test1();
-test2();
+test2()
 
-console.log("last")
+console.log('last')

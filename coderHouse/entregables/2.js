c28b95a592b02b17da0b871b58e069d6aed3088f
montenegrop <pablo.rpm@gmail.com>
2022-05-04 12:54:27 -0300

    fix

--- coderHouse/entregables/2.js
+++ coderHouse/entregables/2.js
@@ -1,53 +1,97 @@
 const fs = require('fs')
-const { usuarioLector1, usuarioLector2 } = require('./1')
 
+// convierte a Json
+const indentado = ' '
 function serialize(object) {
-    return JSON.stringify(object)
+    return JSON.stringify(object, null, indentado)
 }
 
-// function checkEmptyFile(filename) {}
+// devuelve el maximo ID de objetos en un archivo
+function getMaxId(filename) {
+    const ids = getFileObjects(filename).map((object) => object.id)
+    const idMax = Math.max(...ids)
+    return idMax > 0 ? idMax : 0
+}
+
+// recupera los objetos de un archivo
+function getFileObjects(filename) {
+    try {
+        const fileContent = fs.readFileSync(filename, 'utf-8')
+        return JSON.parse(fileContent)
+    } catch (error) {
+        //Posibles errroes: archivo no existe, está vacio o no es JSON valido
+        fs.writeFileSync(filename, '[]', 'utf-8')
+        return []
+    }
+}
 
 class Contenedor {
     constructor(filename) {
         this.filename = filename
-        this.totalObjects = 0
-    }
-    save(objeto) {
-        const methodName = 'save'
-        try {
-            fs.appendFile(this.filename, serialize(objeto) + '\n', (error) => {
-                if (error) {
-                    console.log('error de ' + methodName, error)
-                } else {
-                    console.log('exito de ' + methodName)
-                    this.totalObjects = this.totalObjects + 1
-                    console.log(this.totalObjects)
-                    return this.totalObjects
-                }
-            })
-        } catch (error) {
-            console.log('error al llamar al metodo ' + methodName, error)
+        this.init = function () {
+            this.baseId = getMaxId(filename)
         }
+        this.init()
+    }
+
+    writeInFile(objects, shift) {
+        try {
+            fs.writeFileSync(this.filename, serialize(objects))
+            this.baseId = this.baseId + shift
+        } catch (error) {
+            return 'error de escritura'
+        }
+    }
+
+    save(objeto) {
+        const objetosArray = getFileObjects(this.filename)
+        objeto.id = this.baseId + 1
+        objetosArray.push(objeto)
+        this.writeInFile(objetosArray, 1)
+    }
+
+    getById(id) {
+        return getFileObjects(this.filename).find((object) => object.id === id)
+    }
+    getAll() {
+        return getFileObjects(this.filename)
+    }
+    deleteById(id) {
+        const objects = getFileObjects(this.filename).filter(
+            (object) => object.id != id
+        )
+        this.writeInFile(objects, 0)
+    }
+    deleteAll() {
+        fs.writeFileSync(this.filename, '')
     }
 }
 
-// const objeto = objetoImportado
-const archivo = './2.prueba.txt'
-const elemento1 = [
-    'pablo1',
-    'montenegro1',
-    [{ nombre: 'Fitzgerald1', autor: 'Gatsby1' }],
-    ['perro1', 'gato1', 'pajaro1'],
-]
-const elemento2 = [
-    'pablo1',
-    'montenegro1',
-    [{ nombre: 'Fitzgerald1', autor: 'Gatsby1' }],
-    ['perro1', 'gato1', 'pajaro1'],
-]
-
-cont1 = new Contenedor(archivo)
-cont1.save(elemento1)
-cont1.save(elemento2)
-
-// puede pasar que elemento2 y elemento1 se mezclen al escribirse en el archivo?
+//data pruebas:
+const archivo = './productos.txt'
+const objeto1 = {
+    title: 'titulo1',
+    price: 10,
+    thumbnail: 'www.thum1.com',
+}
+const objeto2 = {
+    title: 'titulo2',
+    price: 20,
+    thumbnail: 'www.thum2.com',
+}
+const objeto3 = {
+    title: 'titulo3',
+    price: 30,
+    thumbnail: 'www.thum3.com',
+}
+// llamados prueba:
+const cont = new Contenedor(archivo)
+cont.save(objeto1)
+cont.save(objeto2)
+cont.save(objeto3)
+console.log('object of id 2:', cont.getById(2))
+cont.getAll()
+cont.deleteById(1)
+console.log('all objects menos el de id 1', cont.getAll())
+cont.save(objeto2)
+cont.deleteAll()

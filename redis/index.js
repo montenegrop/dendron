9a9ee20667b0cd45e0e629c9f933b33d3802a4d0
montenegrop <pablo.rpm@gmail.com>
2022-05-18 18:58:47 -0300

    redis2

--- redis/index.js
+++ redis/index.js
@@ -1,13 +1,28 @@
 import { createClient } from 'redis'
 
+const format = 'redis[s]://[[username][:password]@][host][:port][/db-number]'
+const example = 'redis://alice:foobared@awesome.redis.server:6380'
+
+const url = 'redis-19817.c17.us-east-1-4.ec2.cloud.redislabs.com:19817'
+const host = 'redis-19817.c17.us-east-1-4.ec2.cloud.redislabs.com'
+const user = 'lector'
+const pass = 'Lectura1+'
+
+const customUrl =
+    'redis://lector:Lectura2+@redis-19817.c17.us-east-1-4.ec2.cloud.redislabs.com:19817'
+
 const client = createClient({
-    url: 'redis-19817.c17.us-east-1-4.ec2.cloud.redislabs.com:19817',
-    password: 'nC6WdDnB2YPe4VVEUfHmIY5JCmESFgir',
+    url: customUrl,
 })
+
+await client.connect()
+
+console.log('as')
 
 // client.on('error', (err) => console.log('Redis Client Error', err))
 
-// await client.connect()
+// await client.set('key2', 'value3')
+console.log('as2')
+const value = await client.get('hello')
 
-// await client.set('key', 'value')
-// const value = await client.get('key')
+console.log(value)
